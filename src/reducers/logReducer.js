f0ef21a043b8346bcad4d4ddc3dfaf4f57380897
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2022-02-13 14:49:50 +1000

    Delete Logs

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,4 @@
-import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG} from '../actions/types';
+import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG} from '../actions/types';
 
 
 
@@ -22,6 +22,12 @@ export default(state = initialState, action) => {
                 ...state,
                 logs: [...state.logs, action.payload],
                 loading: false,
+            }
+        case DELETE_LOG:
+            return {
+                ...state,
+                logs: state.logs.filter(log => log.id !== action.payload),
+                loading:false,
             }
         case SET_LOADING:
            return {
